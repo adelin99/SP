291810144e3fb2c87bfef9a16ec45d2f1ca79af2
adelin99 <57272121+adelin99@users.noreply.github.com>
2019-11-01 22:58:18 +0200

    Add files via upload

---
+++ T1_E2_Idor_Adelin.m
@@ -0,0 +1,5 @@
+
+    a=randn(4)
+    index= a<0; %cream un index logic pentru a separa valorile pozitive de cele negative
+    negatives= a(index) %punem in variabila negatives toate valorile <0 din a
+
